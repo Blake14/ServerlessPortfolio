54b865c99bbbda641b487768c924d498c3310889
Blake14 <blakemcpherson2@gmail.com>
2023-01-05 21:19:57 -0600

    data: dummy skill and project data

---
+++ client/src/data/SkillsData.js
@@ -0,0 +1,32 @@
+export const SkillsData = [
+	{
+		id: 1,
+		name: 'JavaScript',
+		badgeColor: '#ffb703',
+		skillLevel: 9,
+	},
+	{
+		id: 2,
+		name: 'AWS',
+		badgeColor: '#dda15e',
+		skillLevel: 9,
+	},
+	{
+		id: 3,
+		name: 'ReactJS',
+		badgeColor: '#f5cac3',
+		skillLevel: 9,
+	},
+	{
+		id: 4,
+		name: 'CSS',
+		badgeColor: '#90e0ef',
+		skillLevel: 9,
+	},
+	{
+		id: 5,
+		name: 'HTML',
+		badgeColor: '#a8dadc',
+		skillLevel: 9,
+	},
+];
